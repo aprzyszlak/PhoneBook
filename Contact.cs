9d7ad47d0c30e3894dbe565caa55bd2ce11891d8
Adrian Przyszlak <adrianprzyszlak@outlook.com>
2023-05-02 17:43:47 +0200

    Add project files.

---
+++ Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PhoneBook
+{
+    class Contact
+    {
+        public Contact(string name, int number)
+        {
+            Number = number;
+            Name = name;
+        }
+
+        public int Number { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Phone Book application, check the option you want to:");
+            Console.WriteLine("1. Add new contact");
+            Console.WriteLine("2. Show contact per number");
+            Console.WriteLine("3. Show all contacts");
+            Console.WriteLine("4. Show contact per name");
+            
+            var userInput = Console.ReadLine();
+            if (userInput != null) 
+            {
+                if (userInput == "1")
+                {
+                    Console.WriteLine("1. Add new contact");
+                    Console.WriteLine("Type name:");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Type nuber:");
+                    int number = int.Parse(Console.ReadLine());
+                    Contact contact = new Contact(name, number);
+                }
+                if (userInput == "2")
+                {
+                    Console.WriteLine("2. Show contact per number");
+                }
+                if (userInput == "3")
+                {
+                    Console.WriteLine("3. Show all contacts");
+                }
+                if (userInput == "4")
+                {
+                    Console.WriteLine("4. Show contact per name");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect value"); 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect value");
+            }
+
+
+            
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
