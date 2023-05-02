597beee4c8caea48856a1fce36e3de426692deb1
Adrian Przyszlak <adrianprzyszlak@outlook.com>
2023-05-02 21:11:21 +0200

    Crudy, ListDatabase

---
+++ ListDatabase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PhoneBook
+{
+    class ListDatabase
+    {
+        static List<Contact> list = new List<Contact>();
+
+        static public List<Contact> AddContact(Contact contact)
+        {
+            list.Add(contact);
+            return list;
+        }
+
+        static public void ShowAll()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine($"Name: {item.Name}, Number: {item.Number}");
+            }
+        }
+
+        static public void GetContactPerNumber(int number)
+        {
+            List<Contact> nowa = new List<Contact>();
+            nowa = list.Where(e => e.Number == number).ToList();
+            foreach (var item in nowa)
+            {
+                Console.WriteLine($"Name: {item.Name}, Number: {item.Number}");
+            }
+        }
+
+        static public void GetContactPerName(string name)
+        {
+            List<Contact> nowa = new List<Contact>();
+            nowa = list.Where(e => e.Name == name).ToList();
+            foreach (var item in nowa)
+            {
+                Console.WriteLine($"Name: {item.Name}, Number: {item.Number}");
+            }
+        }
+
+        static public void DeleteContact(string name)
+        {
+            var contactToDelete = list.FirstOrDefault(contact => contact.Name == name);
+            if (contactToDelete != null)
+            {
+                list.Remove(contactToDelete);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,45 +10,75 @@ namespace PhoneBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Phone Book application, check the option you want to:");
-            Console.WriteLine("1. Add new contact");
-            Console.WriteLine("2. Show contact per number");
-            Console.WriteLine("3. Show all contacts");
-            Console.WriteLine("4. Show contact per name");
-            
-            var userInput = Console.ReadLine();
-            if (userInput != null) 
+            while (true)
             {
-                if (userInput == "1")
+                Console.WriteLine();
+                Console.WriteLine("Welcome to Phone Book application, check the option you want to:");
+                Console.WriteLine("1. Add new contact");
+                Console.WriteLine("2. Show contact per number");
+                Console.WriteLine("3. Show all contacts");
+                Console.WriteLine("4. Show contact per name");
+                Console.WriteLine("5. Remove contact");
+
+                var userInput = Console.ReadLine();
+                if (userInput != null)
                 {
-                    Console.WriteLine("1. Add new contact");
-                    Console.WriteLine("Type name:");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("Type nuber:");
-                    int number = int.Parse(Console.ReadLine());
-                    Contact contact = new Contact(name, number);
-                }
-                if (userInput == "2")
-                {
-                    Console.WriteLine("2. Show contact per number");
-                }
-                if (userInput == "3")
-                {
-                    Console.WriteLine("3. Show all contacts");
-                }
-                if (userInput == "4")
-                {
-                    Console.WriteLine("4. Show contact per name");
+                    if (userInput == "1")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("1. Add new contact");
+                        Console.WriteLine("Type name:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Type nuber:");
+                        int number = int.Parse(Console.ReadLine());
+                        Contact contact = new Contact(name, number);
+                        ListDatabase.AddContact(contact);
+                    }
+                    else if (userInput == "2")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("2. Show contact per number");
+                        Console.WriteLine("Type number to find:");
+                        int number = int.Parse(Console.ReadLine());
+                        ListDatabase.GetContactPerNumber(number);
+                    }
+                    else if (userInput == "3")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("3. Show all contacts");
+                        ListDatabase.ShowAll();
+
+                    }
+                    else if (userInput == "4")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("4. Show contact per name");
+                        Console.WriteLine("Type contact to find:");
+                        string name = Console.ReadLine();
+                        ListDatabase.GetContactPerName(name);
+                    }
+                    else if (userInput == "5")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("5. Remove contact:");
+                        Console.WriteLine("Type contact name to remove:");
+                        string name = Console.ReadLine();
+                        ListDatabase.DeleteContact(name);
+                        ListDatabase.ShowAll();
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Incorrect value");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Incorrect value"); 
+                    Console.Clear();
+                    Console.WriteLine("Incorrect value");
                 }
             }
-            else
-            {
-                Console.WriteLine("Incorrect value");
-            }
+            
 
 
             
